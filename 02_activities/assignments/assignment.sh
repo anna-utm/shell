ac06c073e0c60966685c90e1ec776a481025904e
anna-utm <annautm2023@gmail.com>
2025-08-01 11:43:25 -0400

    Made additional changes to Q2

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -34,7 +34,7 @@ mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
 mkdir -p ./data/raw
 mv ./rawdata/* ./data/raw/
-
+mv ./rawdata .data/raw
 
 # 3. List the contents of the ./data/raw directory
 ls data/raw
